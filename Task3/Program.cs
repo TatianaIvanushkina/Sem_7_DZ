adb1c3e8e62b23886d430a5191c74565f8cde4b1
Tatiana Ivanyshkina <Ita86@bk.ru>
2024-02-21 22:29:42 +0400

    Finished Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+void PrintReverseArray (int[] array, int j)
+{
+    if (j == 0)
+    {
+    Console.WriteLine(array[j]);
+    return;
+    }
+    Console.Write(array[j] + " ");
+    PrintReverseArray (array, j - 1);
+}
+int[] arr = { 1, 2, 5, 0, 10, 34 };
+int j = arr.Length - 1;
+PrintReverseArray (arr, j);
+
